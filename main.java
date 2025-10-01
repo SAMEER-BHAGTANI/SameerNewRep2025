4db2c9423085bd8015d235ab183362542b059d4f
SAHIL-B-007 <114509855+SAHIL-B-007@users.noreply.github.com>
2025-10-01 11:08:59 +0530

    Update main.java
    errors solved in java

--- main.java
+++ main.java
@@ -1,8 +1,8 @@
 public class hi1
 {
-public static void main(string[] args)
-{
-system.out.println("Welcome")
-system.out.println("Java Language")
-}
+public static void main(String[] args)
+  {
+    System.out.println("Welcome");
+    System.out.println("Java Language");
+  }
 }
